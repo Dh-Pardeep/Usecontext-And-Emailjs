c4283503327a160ee948ca6b559962211f9d09d1
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-09-20 11:30:17 +0530

    usecontext

---
+++ src/DataTransfer.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const ScreenFixed = createContext();
+
+export default function ScreenFixedProvider({ children }) {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+  const [show, setShow] = useState(true);
+  const newObj = {
+    name: "abc",
+    age: 123,
+    description: "this is for testing"
+  };
+  const value = { arr, show, newObj, setShow, };
+  return <ScreenFixed.Provider value={value}>{children}</ScreenFixed.Provider>;
+}  
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ScreenFixedProvider from './DataTransfer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ScreenFixedProvider>
+      <App />
+    </ScreenFixedProvider>
   </React.StrictMode>
 );
 
