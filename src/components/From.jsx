6c470bba759b5241691a22a6f390a15e57b02821
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-09-21 12:25:10 +0530

    last commit

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -19,16 +19,15 @@ const From = () => {
   const successAlert = () => {
     Swal.fire({
       title: 'Are you sure?',
-      text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, submitted it!'
+      confirmButtonText: 'Yes, Submitted it!'
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire(
-          'submitted!',
+          'Submitted!',
           'Your file has been submitted.',
           'success'
         )
