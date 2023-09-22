1ed7b507443862bb6dc1e7b6b072854c399732a7
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-09-22 11:45:17 +0530

    final commit

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -8,15 +8,6 @@ const From = () => {
     e.preventDefault();
 
     emailjs.sendForm('service_col123h', 'template_z0ktny5', form.current, 'HLJjgMOe-RolOfJEM')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-    e.target.reset()
-  };
-
-  const successAlert = () => {
     Swal.fire({
       title: 'Are you sure?',
       icon: 'warning',
@@ -29,28 +20,34 @@ const From = () => {
         Swal.fire(
           'Submitted!',
           'Your file has been submitted.',
-          'success'
-        )
-      }
-    })
-  }
-  
+          'Success'
+        );
+      };
+    }
+    );
+    e.target.reset()
+  };
+
+  const successAlert = () => {
+   
+  };
+
   return (
-   <div className='min-vh-100 d-flex align-items-center justify-content-center'>
-     <div className='container_box p-5 '>
-      <div className='small_box'>
-        <form ref={form} onSubmit={sendEmail} className='d-flex flex-column p-4'>
-          <label className='text-white py-2'>Name</label>
-          <input required placeholder='Name' type="text" name="user_name" />
-          <label className='text-white py-2'>Email</label>
-          <input required placeholder='Email' type="email" name="user_email" />
-          <label className='text-white py-2'>Message</label>
-          <textarea className='resizetext' name="message" />
-          <input className='mt-3' required placeholder='Message' onClick={successAlert} type="submit" value="Send" />
-        </form>
+    <div className='min-vh-100 d-flex align-items-center justify-content-center '>
+      <div className='container_box p-5  position-relative z-2 '>
+        <div className='small_box'>
+          <form ref={form} onSubmit={sendEmail} className='d-flex flex-column p-4'>
+            <label className='text-white py-2'>Name</label>
+            <input required placeholder='Name' type="text" name="user_name" />
+            <label className='text-white py-2'>Email</label>
+            <input required placeholder='Email' type="email" name="user_email" />
+            <label className='text-white py-2'>Message</label>
+            <textarea className='resizetext' name="message" />
+            <input className='mt-3' required placeholder='Message' onClick={successAlert} type="submit" value="Send" />
+          </form>
+        </div>
       </div>
     </div>
-   </div>
   )
 }
 
